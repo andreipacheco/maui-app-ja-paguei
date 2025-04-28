1a4a8e1c7316c074ab3c2bd574ab54847adfae56
Andrei B. Pacheco <andrei_pacheco@yahoo.com.br>
2025-04-28 19:23:46 -0300

    Implementado Push Notifications com OneSignal

--- Pagamentos/App.xaml.cs
+++ Pagamentos/App.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using CommunityToolkit.Maui.Behaviors;
 using CommunityToolkit.Maui.Core;
+using OneSignalSDK.DotNet;
+using OneSignalSDK.DotNet;
+using OneSignalSDK.DotNet.Core;
+using OneSignalSDK.DotNet.Core.Debug;
 
 namespace Pagamentos
 {
@@ -8,6 +12,20 @@ namespace Pagamentos
         public App()
         {
             InitializeComponent();
+
+            // DOC: https://documentation.onesignal.com/docs/net-sdk-setup
+
+            string oneSignalAppId = Secrets.OneSignalAppId;
+
+            // Enable verbose OneSignal logging to debug issues if needed.
+            OneSignal.Debug.LogLevel = LogLevel.VERBOSE;
+
+            // OneSignal Initialization
+            OneSignal.Initialize(oneSignalAppId);
+
+            // RequestPermissionAsync will show the notification permission prompt.
+            // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 5)
+            OneSignal.Notifications.RequestPermissionAsync(true);
 
             MainPage = new NavigationPage(new MainPage());
         }
